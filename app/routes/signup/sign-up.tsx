4c81cb32f0b9ad53288abd034e7eedf85f3afda8
ZaidShaikh <szaid5146@gmail.com>
2026-08-25 13:50:36 +0530

    Project basic setip

---
+++ app/routes/signup/sign-up.tsx
@@ -0,0 +1,12 @@
+const SignUp = () => {
+  return (
+    <div className='flex flex-col justify-center items-center bg-background1 text-text w-full h-screen'>
+      {/* Card container */}
+      <div className="w-2/3 h-4/5 bg-black rounded-2xl">
+        Hi
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
