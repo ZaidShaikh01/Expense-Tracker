a5a57062624726147a0a9757d8b257ee571d895f
ZaidShaikh <szaid5146@gmail.com>
2026-08-28 20:36:08 +0530

    Profit/Loss bug fixed

--- app/components/Ui/SummaryCard.tsx
+++ app/components/Ui/SummaryCard.tsx
@@ -3,13 +3,18 @@ import { BsGraphDownArrow, BsGraphUpArrow } from 'react-icons/bs';
 type SummaryCardProps = {
   title: string;
   totalAmount: string;
-  gain?: string;
-  loss?: string;
+  netChange: string;
+  isProfit: boolean;
 };
 
-const SummaryCard = ({ title, totalAmount, gain, loss }: SummaryCardProps) => {
+const SummaryCard = ({
+  title,
+  totalAmount,
+  netChange,
+  isProfit,
+}: SummaryCardProps) => {
   return (
-    <div className='flex p-4 flex-col justify-between hover:scale-95 cursor-pointer  transition bg-white shadow w-65 h-31.25 rounded-2xl '>
+    <div className='flex p-4 flex-col justify-between hover:scale-95 cursor-pointer  transition duration-600 ease-in-out bg-white shadow w-65 h-31.25 rounded-2xl '>
       {/* Title */}
       <span className='text-sm text-text'>{title}</span>
       {/* Amound */}
@@ -18,16 +23,27 @@ const SummaryCard = ({ title, totalAmount, gain, loss }: SummaryCardProps) => {
         {Number(totalAmount).toLocaleString()}
       </span>
       {/* Gain */}
-      {gain && (
-        <span className='flex gap-2 items-center text-sm text-success'>
-          +₹{Number(gain).toLocaleString()} this month <BsGraphUpArrow />
-        </span>
-      )}
-      {loss && (
-        <span className='flex gap-2 items-center text-sm text-danger'>
-          -₹{Number(loss).toLocaleString()} this month <BsGraphDownArrow />
-        </span>
-      )}
+
+      {
+        // Here I will check if Im passing either number or the percentage variable depending upon that I will showcase it
+        isProfit ? (
+          <span className='flex gap-2 items-center text-sm text-success'>
+            +
+            {isNaN(Number(netChange))
+              ? netChange
+              : '$' + Number(netChange).toLocaleString()}{' '}
+            this month <BsGraphUpArrow />
+          </span>
+        ) : (
+          <span className='flex gap-2 items-center text-sm text-danger'>
+            -
+            {isNaN(Number(netChange))
+              ? netChange
+              : '$' + Number(netChange).toLocaleString()}{' '}
+            this month <BsGraphDownArrow />
+          </span>
+        )
+      }
     </div>
   );
 };
--- app/routes/Dashboard/index.tsx
+++ app/routes/Dashboard/index.tsx
@@ -1,4 +1,3 @@
-import { BsGraphUpArrow } from 'react-icons/bs';
 import type { Route } from './+types/index';
 import SummaryCard from '~/components/Ui/SummaryCard';
 
@@ -10,10 +9,45 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const summaryCards = [
+    {
+      title: 'Total Balance',
+      totalAmount: '43520',
+      netChange: '211233',
+      isProfit: false,
+    },
+    {
+      title: 'Total Income',
+      totalAmount: '43520',
+      netChange: '211233',
+      isProfit: true,
+    },
+    {
+      title: 'Total Expenses',
+      totalAmount: '43520',
+      netChange: '%23',
+      isProfit: false,
+    },
+    {
+      title: 'Saving Rates',
+      totalAmount: '43520',
+      netChange: '%23',
+      isProfit: true,
+    },
+  ];
   return (
     <div className='flex flex-col py-10 px-40'>
       {/* Summary Cards Row */}
-      <SummaryCard title='Total Balance' totalAmount='45320' gain='21123' />
+      <div className='flex gap-6 flex-wrap shrink'>
+        {summaryCards.map((card) => (
+          <SummaryCard
+            title={card.title}
+            totalAmount={card.totalAmount}
+            netChange={card.netChange}
+            isProfit={card.isProfit}
+          />
+        ))}
+      </div>
 
       {/* Spending Section OverView */}
       {/* Transaction Table */}
