ec5b47d29acc270b91258654924f1e7310dce138
ZaidShaikh <szaid5146@gmail.com>
2026-08-29 20:07:40 +0530

    Chart added

--- app/routes/Dashboard/index.tsx
+++ app/routes/Dashboard/index.tsx
@@ -2,6 +2,25 @@ import type { Route } from './+types/index';
 import SumamryCards from './SummaryCards';
 import DropdownMenu from '~/components/Ui/SimpleDropDown';
 import SpendingSection from './spending_section';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
+
+const data = [
+  { month: 'Jan', income: 50000, expense: 32000 },
+  { month: 'Feb', income: 50000, expense: 28000 },
+  { month: 'Mar', income: 55000, expense: 35000 },
+  { month: 'Apr', income: 50000, expense: 30000 },
+  { month: 'May', income: 60000, expense: 38000 },
+  { month: 'Jun', income: 60000, expense: 42000 },
+];
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -45,7 +64,47 @@ export default function Home() {
 
       {/* Spending Section OverView */}
       <SpendingSection />
+      {/* LineChart */}
+      <div className='w-1/2 h-75'>
+        <ResponsiveContainer width='100%' height='100%'>
+          <LineChart data={data}>
+            <CartesianGrid strokeDasharray='3 3' />
 
+            {/* X axis → Months */}
+            <XAxis dataKey='month' />
+
+            {/* Y axis → Numbers */}
+            <YAxis />
+
+            <Tooltip />
+            <Legend />
+
+            {/* Income line */}
+            <Line
+              type='monotone'
+              dataKey='income'
+              name='Income'
+              stroke='#22c55e'
+              strokeWidth={2}
+              dot={{ r: 4 }}
+              activeDot={{ r: 6 }}
+              isAnimationActive={true}
+            />
+
+            {/* Expense line */}
+            <Line
+              type='monotone'
+              dataKey='expense'
+              name='Expense'
+              stroke='#ef4444'
+              strokeWidth={2}
+              dot={{ r: 4 }}
+              activeDot={{ r: 6 }}
+              isAnimationActive={true}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
       {/* Spending Overview Head*/}
 
       {/* Transaction Table */}
