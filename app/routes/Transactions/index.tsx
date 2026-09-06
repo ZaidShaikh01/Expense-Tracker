b04ffdb6402a4ed7ca9e2c8273d5ffb8ba076896
ZaidShaikh <szaid5146@gmail.com>
2026-09-06 20:55:06 +0530

    Tables

--- app/routes/Transactions/index.tsx
+++ app/routes/Transactions/index.tsx
@@ -1,7 +1,40 @@
+import { Link } from 'react-router';
 import Button from '~/components/Button';
 import SimpleDropdown from '~/components/Ui/SimpleDropDown';
 
 const TransactionPage = () => {
+  const tableData = [
+    {
+      transaction: 'Netflix Subscription',
+      category: 'Entertainment',
+      date: '1/2/13',
+      Amount: '+7860',
+    },
+    {
+      transaction: 'Subscription',
+      category: 'Entertainment',
+      date: '1/2/13',
+      Amount: '+7860',
+    },
+    {
+      transaction: 'Netflix ',
+      category: 'Entertainment',
+      date: '1/2/13',
+      amount: '+7860',
+    },
+    {
+      transaction: 'youtube Subscription',
+      category: 'Entertainment',
+      date: '1/2/13',
+      amount: '+7860',
+    },
+    {
+      transaction: 'Netflix insta',
+      category: 'Entertainment',
+      date: '1/2/13',
+      amount: '+7860',
+    },
+  ];
   return (
     <div>
       {/* Heading */}
@@ -79,6 +112,32 @@ const TransactionPage = () => {
             'December',
           ]}
         />
+      </div>
+      {/* Table */}
+      <div className='flex gap-4 flex-col justify-between bg-white shadow w-full rounded-2xl  '>
+        <table className='m-2'>
+          <thead className=' rounded-2xl font-bold  bg-[#E6E6E6]'>
+            <tr>
+              <th className='px-4 py-2 '>Date</th>
+              <th className='px-4 py-2 '>Category</th>
+              <th className='px-4 py-2 '>Description</th>
+              <th className='px-4 py-2 '>Amount</th>
+              <th className='px-4 py-2 '>Type</th>
+              <th className='px-4 py-2 '>Action</th>
+            </tr>
+          </thead>
+          <tbody className='bg-white text-center '>
+            {tableData.map((row) => (
+              <tr className='border-b border-stroke' key={row.transaction}>
+                <td>{row.transaction}</td>
+                <td>{row.category}</td>
+                <td>{row.date}</td>
+                <td>{row.amount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {/* <button onClick={handleAdd}>Add element</button> */}
       </div>
     </div>
   );
